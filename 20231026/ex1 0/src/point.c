132ba0aa71de04c53e10e0e181edc1a265cac1ba
unchainedDavid <afonso.script.sol@gmail.com>
2023-10-27 09:48:12 -0400

    fix: wrong return value

--- 20231026/ex1 0/src/point.c
+++ 20231026/ex1 0/src/point.c
@@ -15,20 +15,20 @@ void destroy_point(Point * p) {
 }
 
 int access_point(Point * p, float * x, float * y) {
-  if (p == NULL) return -1;
+  if (p == NULL) return 0;
   *x = p->x;
   *y = p->y;
   return 1;
 }
 
 int set_point(Point * p, float x, float y) {
-  if (p == NULL) return -1;
+  if (p == NULL) return 0;
   p->x = x;
   p->y = y;
   return 1;
 }
 
 float distance_between_points(Point * p1, Point * p2) {
-  if (p1 == NULL || p2 == NULL) return -1;
+  if (p1 == NULL || p2 == NULL) return 0;
   return (float)sqrt(pow(p1->x - p2->x, 2) + pow(p1->y - p2->y, 2));
 }
